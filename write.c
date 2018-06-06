97e3d458f3c4450bd318eb30d10564bc2d5938dc
pintotomas <tomasp834@gmail.com>
2018-06-06 03:57:17 -0300

    Agrego vga_write
    Falta:
    
    - Manejarlo si 'linea' es menor a 0 
    - Ver que este bien como se calcula la posicion, no estoy seguro

---
+++ write.c
@@ -0,0 +1,19 @@
+#define VGABUF ((volatile char *) 0xb8000)
+
+void vga_write(const char *s, int8_t linea, uint8_t color){
+  
+    volatile char *buf = VGABUF;
+    
+    if(linea < 0){
+        //..
+    }
+
+    //else
+    buf += linea;
+    int pos = 0;
+    while (s[pos] != '\0') {
+        *buf++ = s[pos];
+        *buf++ = color;
+        pos++;
+    }   
+}
