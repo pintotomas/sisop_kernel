b05e15a6a79ac00fe924e7fbe238af443b640818
bernardoAntonioOmar <uton.uton@gmail.com>
2018-06-10 19:37:57 -0300

    Se imprime los parámetros pasados desde el booteo usando las librerias provistas por la cátedra

--- kern2.c
+++ kern2.c
@@ -1,8 +1,20 @@
 #include "decls.h"
 #include "multiboot.h"
+#include "lib/string.h"
+
 void kmain(const multiboot_info_t *mbi) {
+    char *cmdline = (void *) mbi->cmdline;
     vga_write("kern2 loading.........", 8, 0x70);
+
+
+        char buf[12] = "cmdline: ";
+        
+        strlcat( buf, cmdline, strlen(buf) + strlen(cmdline) + 1);
+    
+        vga_write(buf, 9, 0x07);
+
     while (1){
         __asm__("hlt");
     }
+
 }
