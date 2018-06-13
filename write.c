06a7f14c94c50f6b7c689907f8376ce6a52ae7ef
ventrick <tomasp834@gmail.com>
2018-06-13 00:04:57 -0300

    fmt_int

--- write.c
+++ write.c
@@ -1,6 +1,5 @@
-#include <stdint.h>
 #define VGABUF ((volatile char *) 0xb8000)
-#include "stdint.h"
+#include "decls.h"
 
 void vga_write(const char *s, int8_t linea, uint8_t color){
   
@@ -21,3 +20,36 @@ void vga_write(const char *s, int8_t linea, uint8_t color){
         pos++;
     }   
 }
+
+static size_t anchura(uint64_t val) {
+    uint64_t n = val;
+    size_t cantidad_digitos = 0;
+    while(n > 0) {
+        cantidad_digitos++;
+        n /= 10;
+    }
+    return cantidad_digitos;
+}
+
+//funcion fmt_int para el primer challange
+
+// Escribe en ‘s’ el valor de ‘val’ en base 10 si su anchura
+// es menor que ‘bufsize’. En ese caso devuelve true, caso de
+// no haber espacio suficiente no hace nada y devuelve false.
+bool fmt_int(uint64_t val, char *s, size_t bufsize) {
+
+    size_t long_val = anchura(val);
+    if (long_val >= bufsize) 
+        return false;    
+    size_t posicion = long_val;
+    uint64_t n = val;
+    s[posicion] = '\0';
+    while(n > 0) {
+        posicion--;
+        s[posicion] = (char) ((n % 10) + 48);
+        n /= 10;
+    }
+    return true;
+}
+
+
