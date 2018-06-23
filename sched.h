3280e98402dcccfba9e7758427009a8e3faf2578
ventrick <tomasp834@gmail.com>
2018-06-23 18:44:35 -0300

    struct Taskframe del ej2

--- sched.h
+++ sched.h
@@ -6,8 +6,21 @@ enum TaskStatus {
 };
 
 struct TaskFrame {
-    // Ver ejercicio kern2-spawn más abajo.
-};
+    uint32_t edi;
+    uint32_t esi;
+    uint32_t ebp;
+    uint32_t esp;
+    uint32_t ebx;
+    uint32_t edx;
+    uint32_t ecx;
+    uint32_t eax;
+    /* below here defined by x86 hardware */
+    uint32_t eip;
+    uint16_t cs;
+    uint16_t padding;
+    uint32_t eflags;
+} __attribute__((packed));
+
 
 struct Task {
     uint8_t stack[4096];
