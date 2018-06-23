9dbd86e4bb8c7c4511e1a3d9401e5817c08bce50
ventrick <tomasp834@gmail.com>
2018-06-23 18:35:59 -0300

    kern2-task

--- sched.c
+++ sched.c
@@ -9,7 +9,7 @@ static struct Task *current;
 void sched_init() {
     /*Esta función debe inicializar la variable global current al primer elemento del arreglo global de tareas, 
     y poner su estado en RUNNING.*/
-    //Tasks[0].estado = RUNNING; 
+    Tasks[0].status = RUNNING; 
 }
 
 void spawn(void (*entry)(void)) {
