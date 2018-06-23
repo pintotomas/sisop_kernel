960591079dd7b9ed43416ff31288e0be79058d8d
ventrick <tomasp834@gmail.com>
2018-06-23 18:27:59 -0300

    Update sched.c

--- sched.c
+++ sched.c
@@ -7,7 +7,9 @@ static struct Task Tasks[MAX_TASK];
 static struct Task *current;
 
 void sched_init() {
-    // ...
+    /*Esta función debe inicializar la variable global current al primer elemento del arreglo global de tareas, 
+    y poner su estado en RUNNING.*/
+    //Tasks[0].estado = RUNNING; 
 }
 
 void spawn(void (*entry)(void)) {
