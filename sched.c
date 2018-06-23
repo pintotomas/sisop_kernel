2c23c28803aeb11b71c8a5eceacab6519e842102
ventrick <tomasp834@gmail.com>
2018-06-23 19:30:37 -0300

    spawn incompleto

--- sched.c
+++ sched.c
@@ -33,6 +33,27 @@ void spawn(void (*entry)(void)) {
     if (free_task->status == READY){
         vga_write("Todo bien", 8, 0x70);
     } 
+
+    //3. Inicializar todos sus registros a cero, excepto %cs, %eip y %eflags.
+    //   En particular %eflags debe contener el flag IF, o las interrupciones de reloj
+    //   no se habilitarán al entrar la tarea en ejecución.
+
+    struct TaskFrame *tf = free_task->frame;
+    tf->edi = 0;
+    tf->esi = 0;
+    tf->ebp = 0;
+    tf->esp = 0;
+    tf->ebx = 0;
+    tf->edx = 0;
+    tf->ecx = 0;
+    tf->eax = 0;
+    tf->padding = 0;
+    //Quedan por inicializar cs,eip,eflags, y la stack dentro de free_task?
+    /*
+    tf->cs = ;
+    tf->eip = ;
+    tf->eflags = ;
+    */
 }
 
 void sched(struct TaskFrame *tf) {
